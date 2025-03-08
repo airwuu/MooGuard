02600b263b85c712e8655e63a103911b0bd167db
Hu-Maxwell <dewebiag@gmail.com>
2025-03-08 15:31:10 -0800

    mooo

---
+++ app/dashboard/Irregularities/Irregularities.tsx
@@ -0,0 +1,13 @@
+/*
+irregularities is placed at the top left of the website. it has a title, and 
+under that is a scrollable container. In the scrollable container, it will display 
+multiple CowData. CowData has name, id, health, sickness confidence, and sickness reason. 
+*/ 
+
+export default function Irregularities( ) {
+  return (
+    <>
+      
+    </> 
+  );
+}
