383efdc211900c2322dd2784cdf6db436a1e7827
aaron <aaron@gmail.com>
2025-03-08 02:00:55 -0800

    stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,38 @@
 import Image from "next/image";
+import Link from "next/link"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
 
 export default function Home() {
   return (
+    <>
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem > 
+          <Link href="/dashboard" legacyBehavior passHref>
+            <NavigationMenuLink>
+              Dashboard
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+    </NavigationMenuList>
+    </NavigationMenu>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-[32px] row-start-2 justify-self-start items-center sm:items-start">
+        <main className="flex flex-col gap-[32px] row-start-2  items-center sm:items-start">
           <Image
-            className="dark:invert"
-            src="/next.svg"
+            className="light:invert"
+            src="/cow.png"
             alt="Next.js logo"
-            width={180}
-            height={38}
+            width={360}
+            height={76}
             priority
           />
           <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
@@ -99,5 +122,6 @@ export default function Home() {
         </a>
       </footer>
     </div>
+    </>
   );
 }
