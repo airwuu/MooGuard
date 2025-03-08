69e3fb290a4dca8c4dd517fc4b07827eab229a5a
Hu-Maxwell <dewebiag@gmail.com>
2025-03-08 10:14:54 -0800

    grid (not working)

--- app/dashboard/cow/cow.tsx
+++ app/dashboard/cow/cow.tsx
@@ -1,5 +1,16 @@
+import React from "react";
+
+interface CowProps {
+  x: number;
+  y: number; 
+}
+
+// this is simply a red circle with an x and y
+
 export default function Cow() {
-
-
-
+  return (
+    <div 
+      className="absolute w-6 h-6 bg-red-500 rounded-full"> 
+    </div> 
+  );
 }
