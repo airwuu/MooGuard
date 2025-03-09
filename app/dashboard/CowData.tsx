c7b53365f78e2eb2645ee8fe92f33b79d4a65d93
Hu-Maxwell <dewebiag@gmail.com>
2025-03-08 21:30:13 -0800

    updated irregularities

--- app/dashboard/CowData.tsx
+++ app/dashboard/CowData.tsx
@@ -3,16 +3,20 @@ CowData has name, id, health, sickness confidence, and sickness reason.
 */ 
 import { useContext } from "react"; 
 import CowContext from "./Context/CowContext"; 
+import TimeContext from "./Context/TimeContext";
 
 export default function CowData({ cow }) {
   const cows = useContext(CowContext); 
+
+  const timeContext = useContext(TimeContext);
+  const { time, setTime } = timeContext;
   
   return (
     <>
       <p><b>{cow.name}</b></p> 
       <p>{cow.healthStatus}, {cow.confidence}% confidence </p>
       {/* currently, we're just assuming time = 1. replace with time later */}
-      <p>Position: ({cow.positions[1].x}, {cow.positions[1].y})</p>
+      <p>Position: ({cow.positions[time].x}, {cow.positions[time].y})</p>
     </> 
   );
 }
